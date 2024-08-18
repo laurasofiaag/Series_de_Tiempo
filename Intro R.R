89b6830cd60fa55e52ce51ef5e570b8792b3974d
laaguila <laura.sofia.aguilar@gmail.com>
2024-08-17 18:07:48 -0600

    Intro R

---
+++ Intro R.R
@@ -0,0 +1,23 @@
+
+# intro -------------------------------------------------------------------
+
+
+# Esto es un comentario
+
+1 + 1
+
+
+
+# Asignar variables -------------------------------------------------------
+
+# para asignar variables usamos " <-" (atajo: ALT + "-")
+
+a <- 4
+
+b <- 3
+
+a + b
+
+
+c = 6 # NO UTILIZARLO
+
